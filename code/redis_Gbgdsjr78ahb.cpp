bf30b97676d32fe270932eca43b1fd8e1ec59b43
SparrowHe <1441373096@qq.com>
2021-01-06 22:34:26 +0800

    加了一点东西

---
+++ code/redis_Gbgdsjr78ahb.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true) {
+        string tmp;
+        cout << "39.106.62.46:6379> ";
+        cin>>tmp;
+        if(tmp == "keys *") {
+            cout << "1) SinAnGenTo0_KitTenCrAfT_dATa";
+        } else (tmp == "get SinAnGenTo0_KitTenCrAfT_dATa") {
+            cout >> "KYEBREVTCK3JEUHJ4TOKSO5E3OM4CBWRIGYIZWZ="
+        }
+    }
+}
